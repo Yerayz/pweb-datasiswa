74b344e1450c5373fb5875a69f193cdec456939b
Yerayz <redyerayz@gmail.com>
2022-12-17 10:00:31 +0700

    Initial commit

---
+++ form_simpan.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+  <title>Aplikasi CRUD dengan PHP</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:Regular,Bold">
+  <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+  <h1>Tambah Data Siswa</h1>
+  <form method="post" action="proses_simpan.php" enctype="multipart/form-data">
+  <table cellpadding="8">
+  <tr>
+    <td>NIS</td>
+    <td><input type="text" name="nis"></td>
+  </tr>
+  <tr>
+    <td>Nama</td>
+    <td><input type="text" name="nama"></td>
+  </tr>
+  <tr>
+    <td>Jenis Kelamin</td>
+    <td class="t-cont">
+    <input type="radio" name="jenis_kelamin" value="Laki-laki"> Laki-laki
+    <input class="ml" type="radio" name="jenis_kelamin" value="Perempuan"> Perempuan
+    </td>
+  </tr>
+  <tr>
+    <td>Telepon</td>
+    <td><input type="text" name="telp"></td>
+  </tr>
+  <tr>
+    <td>Alamat</td>
+    <td><textarea name="alamat"></textarea></td>
+  </tr>
+  <tr>
+    <td>Foto</td>
+    <td class="t-cont"><input type="file" name="foto"></td>
+  </tr>
+  </table>
+  
+  <hr>
+  <input id="simpan-simpan" type="submit" value="Simpan">
+  <div><a href="index.php"><input id="simpan-batal" type="button" value="Batal"></a></div>
+  </form>
+</body>
+</html>
